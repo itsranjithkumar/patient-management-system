8686ad149e5a66e72356ba05a4b7197a4ce8e7d2
itsranjithkumar <ranjithkumarms28@gmail.com>
2024-08-02 18:32:20 +0530

    error fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className=' text-white text-3xl underline'>Home</h1>
-      <Button>Click me</Button>
-    </div>
-  )
-}
+   <div className="">
+    <h1 className="text-white text-3xl underline">Home</h1>
+    <Button>Click me</Button>
+   </div>
+  );
+} 
--- lib/utils.ts
+++ lib/utils.ts
@@ -75,4 +75,3 @@ export function encryptKey(passkey: string) {
 export function decryptKey(passkey: string) {
   return atob(passkey);
 }
-
