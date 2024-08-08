64a6c8dadd22e5fca68b2f221ad746824da5d0bc
itsranjithkumar <ranjithkumarms28@gmail.com>
2024-08-08 15:03:00 +0530

    errorfixed

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -23,6 +23,7 @@ import DatePicker from "react-datepicker"
 import "react-datepicker/dist/react-datepicker.css";
 import { Select, SelectContent, SelectTrigger, SelectValue } from "./ui/select";
 import { Textarea } from "./ui/textarea";
+import { Checkbox } from "./ui/checkbox";
 
 interface CustomProps {
   control: Control<any>;
@@ -53,7 +54,7 @@ const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
             className='shad-textArea'
             disabled={props.disabled}
             />
-        </FormControl>
+         </FormControl>
       )
       return (
         <div className="flex rounded-md border border-dark-500">
@@ -139,6 +140,20 @@ const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
 
       case FormFieldType.SKELETON:
         return renderSkeleton ? renderSkeleton(field) : null;
+      case FormFieldType.CHECKBOX:
+       return <FormControl>
+          <div className='flex items-center gap-4'>
+            <Checkbox 
+            id={props.name}
+            checked={field.value}
+            onCheckedChange={field.onChange}
+            />
+            <label htmlFor={props.name}
+            classname="checkbox-label">
+              {props.label}
+              </label>
+          </div>
+        </FormControl>
     default:
       break;
   }
